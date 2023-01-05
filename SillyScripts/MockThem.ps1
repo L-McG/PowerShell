3ecc933d0e0eff3e351a1944bfcef6514c611e28
lucas.mcglamery <lucas.mcglamery.1.ctr@us.af.mil>
2023-01-05 09:34:01 -0600

    Mock them!

---
+++ SillyScripts/MockThem.ps1
@@ -0,0 +1,45 @@
+<#
+.SYNOPSIS
+	Mock them!
+.DESCRIPTION
+    This function iterates through the characters of a string and performs
+    mock casing for letters. The output is piped to the clipboard for ease
+    of delivery and returned to the console.
+.NOTES
+    Version:    v1.0 -- 5 Jan 2023
+	Author:     Lucas McGlamery
+.EXAMPLE
+	PS> Mock-Them "That's not very nice of you"
+#>
+function Mock-Them {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$string
+    )
+    
+    begin {
+        $i = 0
+        $output = $null
+    }
+    
+    process {
+        foreach($character in $string.ToCharArray()) {
+            if ($character -notmatch '^[a-zA-Z]') {
+                $output += $character
+                $i++
+                continue
+            }
+            if (++$i % 2) {
+                $output += $character.ToString().ToUpper()
+            } else {
+                $output += $character.ToString().ToLower()
+            }
+        }
+    }
+    
+    end {
+        $output | clip
+        return $output
+    }
+}
